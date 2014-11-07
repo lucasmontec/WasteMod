fda948e89c79475db008e79f9d3984de71bcbbde
Lucas <lucas_carvalhaes@hotmail.com>
2014-11-07 04:24:53 -0200

    Not a sandbox derived anymore

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -6,9 +6,6 @@ GM.Website 	= "N/A"
 
 include( 'player_meta.lua' )
 
---Lets derive from sanbox
-DeriveGamemode( "sandbox" )
-
 --Default team
 function GM:CreateTeams( )
 	team.SetUp( 1, "Free man", Color( 230, 230, 230 ), false )
